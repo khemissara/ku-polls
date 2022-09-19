288f4a87e6ed107cff4672bc3762772dd30b5fe4
khemissara <khemissara.p@ku.th>
2022-09-19 16:53:02 +0700

    add checking authentication in index.html and signup function in view

--- polls/views.py
+++ polls/views.py
@@ -75,3 +75,20 @@ def vote(request, question_id):
 def redirect_url(request):
     """redirect the url"""
     return HttpResponseRedirect(reverse('polls:index'))
+
+def signup(request):
+    """Register a new user."""
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_passwd = form.cleaned_data.get('password')
+            user = authenticate(username=username,password=raw_passwd)
+            login(request, user)
+            return redirect('polls')
+        # what if form is not valid?
+        # we should display a message in signup.html
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
