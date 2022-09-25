c888fd7fee6194f22fe4c69d882276362a7c1923
Khemissara Pratummanee <khemissara.p@ku.th>
2022-09-25 16:04:05 +0700

    add allow host

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
 import os.path
 import logging
 
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', cast=str, default="missing-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool, default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default='')
 
 
 # Application definition
