51c42171c49c2647059b27406a2b4071302474d0
Khemissara Pratummanee <khemissara.p@ku.th>
2022-10-17 22:28:54 +0700

    import redirect

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice, Vote
 from django.template import loader
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404 , redirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
